17457d8531224a4cb857f196f697a91af0c6c6d5
ericfernandomartinezesparza-ship-it <ericfernandomartinezesparza@gmail.com>
2026-07-15 17:11:24 -0600

    Create Programa3.py

---
+++ Programa3.py
@@ -0,0 +1,12 @@
+calificacion = float(input("Ingresa la calificación: "))
+
+if calificacion >= 90:
+    print("A")
+elif calificacion >= 80:
+    print("B")
+elif calificacion >= 70:
+    print("C")
+elif calificacion >= 60:
+    print("D")
+else:
+    print("F")
